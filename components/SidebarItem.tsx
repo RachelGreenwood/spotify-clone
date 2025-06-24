d20487f6423d3faa8e54ed63d5ba01db9d2de117
Rachel Greenwood <Rachel.Greenwood@sony.com>
2025-06-24 17:05:26 -0500

    Adds Hover CSS to SideBar Items

--- components/SidebarItem.tsx
+++ components/SidebarItem.tsx
@@ -1,8 +1,39 @@
-const SidebarItem = () => {
+import { IconType } from "react-icons";
+import Link from "next/link";
+import { twMerge } from "tailwind-merge";
+
+interface SidebarItemProps {
+    icon: IconType;
+    label: string;
+    active?: boolean;
+    href: string;
+}
+
+const SidebarItem: React.FC<SidebarItemProps> = ({
+    icon,
+    label,
+    active,
+    href
+}) => {
     return (
-        <div>
+        <Link
+        href={href}
+        className={twMerge(`
+        flex
+        flex-row
+        h-auto
+        items-center
+        w-full
+        gap-x-4
+        text-md
+        font-medium
+        cursor-pointer
+        hover:text-white
+        transition
+        text-neutral-400
+        py-1`)}>
             Sidebar Item
-        </div>
+        </Link>
     );
 }
 
