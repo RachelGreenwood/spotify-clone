beba1fd0dfd6e4693a7a2ee213b36bf556edf4ae
Rachel Greenwood <Rachel.Greenwood@sony.com>
2025-09-13 10:10:44 -0500

    Styles Header

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
       <Header>
-        Header
+        <div className="mb-2">
+          <h1 className="text-white text-3xl font-semibold">Welcome Back</h1>
+        </div>
       </Header>
     </div>
   );
