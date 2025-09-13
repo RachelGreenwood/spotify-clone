6724141d48d9b5c7dabdabbe8bdc75bbb21aba58
Rachel Greenwood <Rachel.Greenwood@sony.com>
2025-09-12 22:28:54 -0500

    Adds Mobile View Home Icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from "next/navigation";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { twMerge } from "tailwind-merge";
+import { HiHome } from "react-icons/hi";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -27,6 +28,11 @@ const Header: React.FC<HeaderProps> = ({
                     </button>
                     <button onClick={() => router.forward()}  className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition">
                         <RxCaretRight className="text-white" size={35} />
+                    </button>
+                </div>
+                <div className="flex md:hidden gap-x-2 items-center">
+                    <button>
+                        <HiHome />
                     </button>
                 </div>
             </div>
