9ff3524a6bdb370bfff9a6fc5697f5aa17b54bad
Rachel Greenwood <Rachel.Greenwood@sony.com>
2025-09-12 20:16:13 -0500

    Creates Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,24 @@
-const Header = () => {
+"use client";
+
+import { useRouter } from "next/navigation";
+
+interface HeaderProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Header: React.FC<HeaderProps> = ({
+    children,
+    className
+}) => {
+    const router = useRouter();
+
+    const handleLogout = () => {
+        //Handle logout in the future
+    }
     return (
         <div>
-            Hello Header!
+            //
         </div>
     );
 }
