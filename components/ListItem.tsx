883ed0b64da051e236457586f3934ebbe372394f
Rachel Greenwood <Rachel.Greenwood@sony.com>
2025-09-13 10:17:54 -0500

    Creates ListItem Button

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,8 +1,31 @@
-const ListItem = () => {
+"use client";
+
+import { useRouter } from "next/navigation";
+
+interface ListItemProps {
+    image: string;
+    name: string;
+    href: string;
+}
+
+const ListItem: React.FC<ListItemProps> = ({
+    image,
+    name,
+    href
+}) => {
+    const router = useRouter();
+
+    const onClick = () => {
+        //Add authenthication before push
+        router.push(href);
+    }
+
     return (
-        <div>
-            List Item!
-        </div>
+        <button className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
+            <div className="relative min-h-[64px] min-w-[64px]">
+                
+            </div>
+        </button>
     );
 }
 
