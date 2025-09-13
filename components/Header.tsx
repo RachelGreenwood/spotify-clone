2c3a9a119ddb15eb48ad71bbdc44d4ddc8f7f259
Rachel Greenwood <Rachel.Greenwood@sony.com>
2025-09-12 22:23:11 -0500

    Adds Left Arrow Icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { RxCaretLeft } from "react-icons/rx";
 import { twMerge } from "tailwind-merge";
 
 interface HeaderProps {
@@ -19,6 +20,13 @@ const Header: React.FC<HeaderProps> = ({
     }
     return (
         <div className={twMerge(`h-fit bg-gradient-to-b from-emerald-800 p-6`, className)}>
+            <div className="w-full mb-4 flex items-center justify-between">
+                <div className="hidden md:flex gap-x-2 items-center">
+                    <button>
+                        <RxCaretLeft />
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
