56b5f1094acc81309f8558d71b5af9725205cf17
Rachel Greenwood <Rachel.Greenwood@sony.com>
2025-09-13 10:25:20 -0500

    Styles Liked Songs Text

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -26,7 +26,7 @@ const ListItem: React.FC<ListItemProps> = ({
             <div className="relative min-h-[64px] min-w-[64px]">
                 <Image className="object-cover" fill src={image} alt="Image" />
             </div>
-            <p>{name}</p>
+            <p className="font-medium truncate py-5">{name}</p>
         </button>
     );
 }
